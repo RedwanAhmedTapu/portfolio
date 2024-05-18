9228bcbd6d1ae221fc37773c3e447798622b0a25
RedwanAhmedTapuu <redwan25880@gmail.com>
2024-05-18 16:12:31 +0600

    env file updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from "@vitejs/plugin-react";
-import dns from "dns";
+// import dns from "dns";
 
-dns.setDefaultResultOrder("verbatim");
+// dns.setDefaultResultOrder("verbatim");
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
